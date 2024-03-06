9370b78fa14e45d2ad49435862bdee5b2c3c513f
ayushsingh953 <ayushsingh35323@gmail.com>
2024-03-06 12:10:52 +0530

    updated error handler

--- auth/src/middlewares/errorHandler.ts
+++ auth/src/middlewares/errorHandler.ts
@@ -4,6 +4,6 @@ export const errorHandler=(err:Error,req:Request,res:Response,next:NextFunction)
 
     console.log("Something went wrong");
 
-    res.status(400).send({message:"Something went wrong"});
+    res.status(400).send({message:err.message});
 
 }
