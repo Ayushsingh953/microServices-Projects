fd5fd2ab4d5c67b9a6c78b326c39ef040489a323
ayushsingh953 <ayushsingh35323@gmail.com>
2024-03-08 14:47:28 +0530

    Connected to Db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose"; 
+
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -22,7 +24,16 @@ app.all('*',async()=>{
 
 app.use(errorHandler);
 
+const start = async()=>{
+    try{
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to DB");
+    }catch(err){
+        console.error(err);
+    }
+    app.listen(3000,()=>{
+        console.log("Listening on port 3000!");
+    });
+}
 
-app.listen(3000,()=>{
-    console.log("Listening on port 3000!");
-});
+start();
